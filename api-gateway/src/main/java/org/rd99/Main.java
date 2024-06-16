4a1d6f26d390c630eae05fa799e864d56db54efc
rd-99 <ruturaj.dhakane@iqvia.com>
2024-06-16 10:59:53 +0530

    microservices

--- api-gateway/src/main/java/org/rd99/Main.java
+++ api-gateway/src/main/java/org/rd99/Main.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
